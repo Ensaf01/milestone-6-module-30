e1f1d08ce6a0613d653e498ddc140d297ea80038
ensaf01 <mohammedensaf402@gmail.com>
2026-08-12 08:02:18 +0600

    use backtick `

---
+++ template_literals.js
@@ -0,0 +1,17 @@
+const a=10;
+const b=20;
+const result="the sum is "+ a +" and "+ b+" result is = " + (a + b);
+console.log(result);
+
+console.log(`The value of a is ${a} and the value of b is ${b} and the sum = ${a + b}`);
+
+
+//normal string
+const string1="hello world \n this is a multi line string \n using normal string";
+console.log(string1);
+
+// multi line string using template literals
+const string=`hello world
+this is a multi line string
+using template literals`;
+console.log(string);
